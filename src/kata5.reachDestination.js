fec93aad5f9827a4b6068fb5c77db4e23997e5d2
Tim Douglas <me@timdouglas.net>
2020-03-11 07:20:17 +0000

    not sure on the reachDestination one

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,14 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  let hours = 0;
+  let minutes = distance / speed;
+  minutes *= 60;
+
+  if (minutes % 60 === 0) {
+    hours += 1;
+    minutes = 0;
+  }
+
+  return `I will arrive in ${hours} hour ${minutes} minutes`;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,12 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival, using KM and KM/HR", () => {
+    expect(reachDestination(100, 200)).toBe(
+      "I will arrive in 0 hour 30 minutes"
+    );
+    expect(reachDestination(100, 100)).toBe(
+      "I will arrive in 1 hour 0 minutes"
+    );
+  });
 });
